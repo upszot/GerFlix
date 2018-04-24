5bae7a720c3403020469ed6ac69e54d888f3f29d
upszot <upszot@gmail.com>
2018-04-24 19:02:55 -0300

    Documentadas las funciones que agrege..

--- GerFlix 1.0/Serie.h
+++ GerFlix 1.0/Serie.h
@@ -10,5 +10,24 @@ typedef struct
 
 void inicializarSeriesEstado(eSerie[], int);
 void inicializarSeriesHardCode(eSerie[]);
+
+/** \brief mostrarListaSeries
+ *
+ * Recorre todas las series Habilitadas y llama a la funcion que imprime sus datos
+ *
+ * \param eSerie[]
+ * \param int
+ * \return void
+ *
+ */
 void mostrarListaSeries(eSerie[], int);
+
+/** \brief mostrarSerie
+ *
+ * Imprime los datos de un registro de serie
+ *
+ * \param serie eSerie
+ * \return void
+ *
+ */
 void mostrarSerie(eSerie serie);
--- GerFlix 1.0/Usuario.h
+++ GerFlix 1.0/Usuario.h
@@ -9,5 +9,24 @@ typedef struct
 
 void inicializarUsuariosEstado(eUsuario[], int);
 void inicializarUsuariosHardCode(eUsuario[]);
+
+/** \brief mostrarListaUsuarios
+ *
+ * Recorro todos los usuarios Habilitados y llamo a la funcion que imprimo los datos.
+ *
+ * \param eUsuario[]
+ * \param int
+ * \return void
+ *
+ */
 void mostrarListaUsuarios(eUsuario[], int);
+
+/** \brief mostrarUsuario
+ *
+ * Imprimo los datos de un registro de usuario
+ *
+ * \param usuario eUsuario
+ * \return void
+ *
+ */
 void mostrarUsuario(eUsuario usuario);
